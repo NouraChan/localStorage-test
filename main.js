5622b8ecaa6b55de000daf2d65babfe80a2c10c3
norhanali <162762157+NouraChan@users.noreply.github.com>
2024-09-02 18:11:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,102 +2,129 @@ var addBtn = document.getElementById('addBtn');
 var productName = document.getElementById('productName');
 var productId = document.getElementById('productId');
 var productDesc = document.getElementById('productDesc');
-var  productRate = document.getElementById('productRate');
+var productRate = document.getElementById('productRate');
 var quantity = document.getElementById('productQuantity');
 var productPrice = document.getElementById('productPrice');
 
-var Data = [];
 
-addBtn.addEventListener('click' , function(){
+printDataInTable();
+
+
+addBtn.addEventListener('click', function() {
     addProduct();
-})
+});
 
+function addProduct() {
+    var jsonstr = setData();
+    addToLocalStorage(jsonstr);    
+    printDataInTable();
 
-
-function addProduct(){
-  
-  
-  var jsonstr = setData(Data);
-  
-  printDataInTable(Data);
-  addToLocalStorage(jsonstr);
-  //if (addToLocalStorage(Data)==true) {
-    //Data = [];
-//  }else {alert("Error while adding Product.");
-//}
 }
 
+function setData() {
 
-
-
-// debugger;
-function setData(){
-
-    var Object = {
-       id: productId.value,
-       productName : productName.value ,
-       productPrice : productPrice.value,
-       productDesc : productDesc.value,
-       productRate : productRate.value,
-       productQuanity : quantity.value,
-       
+    var object = {
+        id: productId.value,
+        productName: productName.value,
+        productPrice: productPrice.value,
+        productDesc: productDesc.value,
+        productRate: productRate.value,
+        productQuanity: quantity.value,
+        createdAt: new Date().toISOString() // Add creation time
     };
+
+   
+console.log(JSON.stringify(object));
+
+    // console.log(object);
     
-    Data.push(Object);
+    return object;
+}
+
+function addToLocalStorage(object) {    
+  
+  var jsonString = JSON.stringify(object);
+        localStorage.setItem(object.id, jsonString);
     
- 
- var jsonString = JSON.stringify(Data);
+}
 
-//console.log(jsonString); 
-return jsonString;
+function printDataInTable() {
+  var row = ``;
+  var numItems = localStorage.length;
 
+  for (var i = 0; i < numItems; i++) {
+    var key = localStorage.key(i);
+    var product = JSON.parse(localStorage.getItem(key));
+    
+    row += `
+      <tr><td>${product.id}</td>
+      <td>${product.productName}</td> 
+      <td>${product.productPrice}</td>
+      <td>${product.productDesc}</td>
+      <td>${product.productRate}</td>
+      <td>${product.productQuanity}</td>
+      <td><button type="button" class="btn btn-primary float-end my-3" data-id="${product.id}" id="updBtn">Update</button></td>
+      <td><button type="button" class="btn btn-primary float-end my-3" data-id="${product.id}" id="delBtn">Delete</button></td>
+      </tr>`;
+  }
+
+  document.getElementById('table').innerHTML = row;
 }
 
 
 
-function addToLocalStorage(jsonstr){
-
-id = 0;
-for (var i = 0; i < Data.length; i++) {
-  localStorage.setItem(id, jsonstr);
-
-instorage= localStorage.getItem(id);  
-
-
-console.log(instorage);
-
-id++
-
-}}
+var delBtns = document.querySelectorAll('[data-id]');;
+for (var i = 0; i < delBtns.length; i++) {
+  delBtns[i].addEventListener('click', function() {
+    deleteItem(this.dataset.id); // Use `this.dataset.id` to get the product ID
+  });
+}
 
 
+function deleteItem(id) {
+localStorage.removeItem(id);
 
-function printDataInTable(data) {
-    var row = ``;
-    for (let i = 0; i < data.length; i++) {
-        row += `
-        <tr><td>${data[i].id}</td>
-        <td>${data[i].productName}</td> 
-        <td>${data[i].productPrice}</td>
-        <td>${data[i].productDesc}</td>
-        <td>${data[i].productRate}</td>
-        <td>${data[i].productQuanity}</td>
-        <td><button type="button" class="btn btn-primary float-end my-3">Update</button><td>
-        <td><button type="button" class="btn btn-primary float-end my-3" id="delBtn">Delete</button></td>
-        </tr>`
-       
-    }
-    document.getElementById('table').innerHTML=row;
+
+// Remove the deleted product from the Data array as well
+
+printDataInTable();
 }
 
 
 
-function deleteItem(id){
-  var delBtn = document.getElementById('delBtn');
 
-delBtn.addEventListener('click' , function (){
-  localStorage.removeItem(id);
-})
 
+
+
+
+function updateItem(id) {
+  var updateBtn = document.querySelector('#updBtn');
+
+  updateBtn.addEventListener('click', function() {
+    // Get the updated product data from the form fields
+    var updatedProductName = document.getElementById('productName').value;
+    var updatedProductPrice = document.getElementById('productPrice').value;
+    var updatedProductDesc = document.getElementById('productDesc').value;
+    var updatedProductRate = document.getElementById('productRate').value;
+    var updatedProductQuantity = document.getElementById('productQuantity').value;
+
+    // Create an updated product object
+    var updatedProduct = {
+      id: id,
+      productName: updatedProductName,
+      productPrice: updatedProductPrice,
+      productDesc: updatedProductDesc,
+      productRate: updatedProductRate,
+      productQuanity: updatedProductQuantity
+    };
+
+    // Update the product in local storage
+    localStorage.setItem(id, JSON.stringify(updatedProduct));
+
+    // Update the product in the Data array
+    Data = Data.map(product => product.id === id ? updatedProduct : product);
+
+    // Update the table to reflect the changes
+    printDataInTable(Data);
+  });
 }
-
